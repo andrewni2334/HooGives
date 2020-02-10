9f6b8716218e02cd33eb7f3f59e79e779c2d6830
Kush <kp2zf@virginia.edu>
2020-02-09 22:06:28 -0500

    event create/delete
    event create/delete

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path('api/v1/users/', views.user_api),
-    path('api/v1/events/', views.event_api)
+    path('api/v1/events/', views.event_api),
+    path('api/v1/events/create/', views.event_create_api),
+    path('api/v1/events/delete/', views.event_delete_api)
 ]
